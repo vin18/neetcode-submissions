e772d25dabc8386aa5f5aa5828149726ba1c067a
Vinit Raut <31928236+vin18@users.noreply.github.com>
2026-05-29 01:50:29 +0530

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} digits
+     * @return {string[]}
+     */
+    letterCombinations(digits) {
+        const result = [];
+        if (digits.length === 0) return result;
+        const digitsMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        };
+
+        function backtrack(index, currChar) {
+            if (index === digits.length) {
+                result.push(currChar);
+                return;
+            }
+            const characters = digitsMap[digits[index]];
+            for (const c of characters) {
+                backtrack(index + 1, currChar + c);
+            }
+        }
+        backtrack(0, "");
+
+        return result;
+    }
+}
